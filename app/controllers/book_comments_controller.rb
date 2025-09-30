ce621cc1fc91c1d86daf37cdd99868086e86e491
EC2 Default User <ec2-user@ip-172-31-32-245.ap-northeast-1.compute.internal>
2025-09-30 07:23:34 +0000

    [Finish] いいねのソート機能

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -5,12 +5,11 @@ class BookCommentsController < ApplicationController
     comment = current_user.book_comments.new(book_comment_params)
     comment.book_id = book.id
     comment.save
-    redirect_to request.referer
   end
 
   def destroy
-    BookComment.find_by(id: params[:id], book_id: params[:book_id]).destroy
-    redirect_to request.referer
+    @comment = BookComment.find_by(id: params[:id], book_id: params[:book_id])
+    @comment.destroy
   end
 
   private
--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,40 +1,16 @@
 class FavoritesController < ApplicationController
 
   def create
-    @book = Book.find(params[:book_id])
-    @favorite = current_user.favorites.new(book_id: @book.id)
+    book = Book.find(params[:book_id])
+    @favorite = current_user.favorites.new(book_id: book.id)
     @favorite.save
-
-    # 呼び出し元で表示範囲を切り替え
-    if params[:user_id].present?
-      @user = User.find(params[:user_id])
-      @books = @user.books.order(favorites_count: :desc)
-    else
-      @user = nil
-      @books = Book.order(favorites_count: :desc)
-    end
-
-    respond_to do |format|
-      format.js
-    end
+    render 'replace_btn'
   end
 
   def destroy
-    @book = Book.find(params[:book_id])
-    @favorite = current_user.favorites.find_by(book_id: @book.id)
+    book = Book.find(params[:book_id])
+    @favorite = current_user.favorites.find_by(book_id: book.id)
     @favorite.destroy
-
-    # 呼び出し元で表示範囲を切り替え
-    if params[:user_id].present?
-      @user = User.find(params[:user_id])
-      @books = @user.books.order(favorites_count: :desc)
-    else
-      @user = nil
-      @books = Book.order(favorites_count: :desc)
-    end
-
-    respond_to do |format|
-      format.js
-    end
+    render 'replace_btn'
   end
 end
